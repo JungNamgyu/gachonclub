99c243580c4e8f5c7fa693211d13220bc311402c
Namgyu <jng6017@naver.com>
2020-01-09 23:14:11 +0900

    Button Add

--- app/src/main/java/com/example/gachon_club/MainActivity.kt
+++ app/src/main/java/com/example/gachon_club/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gachon_club
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gachon_club.ui.login.LoginActivity
+import com.example.gachon_club.ui.login.SignUp
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -13,7 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Main_club.setOnClickListener{ startActivity<Main_Club>() }
-        Login.setOnClickListener{ startActivity<LoginActivity>() }
+        Main_club.setOnClickListener{
+            Title.text = "중앙 동아리"
+            startActivity<Main_Club>()
+        }
+        sub_club.setOnClickListener{
+            Title.text = "과 동아리"
+            startActivity<Main_Club>()
+        }
+        Sign_in.setOnClickListener{ startActivity<LoginActivity>() }
+        Sign_Up.setOnClickListener{ startActivity<SignUp>() }
     }
 }
