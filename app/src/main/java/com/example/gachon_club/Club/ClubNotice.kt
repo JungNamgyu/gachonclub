b5b0714da34dbef49c99a1d481bf3641d42671f4
JungNamgyu <jng6017@naver.com>
2020-06-22 01:32:45 +0900

    공지사항 클릭리스너 구현

--- app/src/main/java/com/example/gachon_club/Club/ClubNotice.kt
+++ app/src/main/java/com/example/gachon_club/Club/ClubNotice.kt
@@ -1,13 +1,37 @@
 package com.example.gachon_club.Club
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gachon_club.Club.Model.Board
+import com.example.gachon_club.Network.ServiceControl
 import com.example.gachon_club.R
+import kotlinx.android.synthetic.main.activity_notice.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ClubNotice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notice)
-
+        val id = intent.getLongExtra("id", 0)
+        loadData(id)
+    }
+    private fun loadData(id:Long) {
+        val retrofitService = ServiceControl.getInstance()
+        retrofitService?.getBoard(id)?.enqueue(object: Callback<Board> {
+            override fun onResponse(call: Call<Board>, response: Response<Board>) {
+                if (response.isSuccessful) {
+                    val body = response.body()
+                    body?.let {
+                        text_Boardcontent.text = body.content
+                    }
+                }
+            }
+            override fun onFailure(call: Call<Board>, t: Throwable) {
+                Log.d("this is error",t.message)
+            }
+        })
     }
 }
